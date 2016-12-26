45d2b02299dc813374618dc567b1bb3ba06acae3
Nathan Duarte <nduartech101@gmail.com>
2016-12-26 15:24:28 -0500

    second commit

--- app.js
+++ app.js
@@ -3,8 +3,13 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  res.render('tweets');
 });
 
 app.listen(8080, function() {
