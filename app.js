c56c82b5962b938d1bdc11b4435448e121ad51d7
Nathan Duarte <nduartech101@gmail.com>
2016-12-26 21:17:46 -0500

    third commit

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 'use strict'
 
+var mysql = require('mysql');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'vagrant',
+  password: '',
+  database: 'twitter'
+});
+
+connection.connect(function(err) {
+  if(err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Connected to the database.');
+
+  app.listen(8080, function() {
+    console.log('Web server listening on port 8080!');
+  });
+});
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('tweets');
 });
 
-app.listen(8080, function() {
-  console.log('Web server listening on port 8080!');
+app.post('/tweets/create', function(req, res) {
+  var query = 'INSERT INTO Tweets(handle, body) VALUES(?, ?)';
+  var handle = req.body.handle;
+  var body = req.body.body;
+
+  connection.query(query, [handle, body], function(err) {
+    if(err) {
+      console.log(err);
+    }
+
+    res.redirect('/');
+  });
+
+  res.send('Creating tweet.');
 });
